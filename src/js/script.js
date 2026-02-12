67893bee54999a335114fc33f2d99a4f6120bb03
Theon Antwan Beckford <t.a.beckford@se21.qmul.ac.uk>
2026-02-12 19:59:08 +0000

    Added the code for the second page

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,29 @@
 document.getElementById("quiz").addEventListener("submit", function (e) {
     e.preventDefault();
-    alert("Why'd you waste your time?");
+
+    const correct = { 
+        Q1: "saiga antelope", 
+        Q2: "FALSE", 
+        Q3: "W_G",
+        Q4: "green"
+    };
+
+    let score = 0;
+
+    const A1 = this.elements["Q1"].value.trim();
+    if (A1 == correct.Q1) score++;
+
+    const A2 = this.querySelector('input[name="Q2"]:checked');
+    if (A2.value === correct.Q2) score++;
+
+    const A3 = this.querySelector('input[name="Q3"]:checked');
+    if (A3.value === correct.Q3) score++;
+
+    const A4 =  this.elements["Q4"].value.trim();
+    if (A4 == correct.Q4) score++;
+
+    localStorage.setItem("score", score);
+
+    window.location.href = "pages/results.html";
+
 });
