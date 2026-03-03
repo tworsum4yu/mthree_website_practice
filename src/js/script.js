e1e604544976d86d4191cff809ce0b7d179b87fc
Theon Antwan Beckford <t.a.beckford@se21.qmul.ac.uk>
2026-03-03 14:16:16 +0000

    More changes to the quiz

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,118 @@
 // Index page
 const quiz = document.getElementById('quiz');
 
+// Do check to ensure that this works properly
+function parseQuiz(text) {
+  const blocks = text.trim().split('\r\n\r\n');
+  console.log(blocks);
+  return blocks.map((block) => {
+    const lines = block.split('\r\n');
+    const question = lines.find((l) => l.startsWith('Q:')).slice(3);
+    const answers = lines.filter((l) => l.startsWith('A:')).map((a) => a.slice(3));
+    const correct = lines.filter((l) => l.startsWith('C:')).map((c) => c.slice(3));
+    return { question, answers, correct };
+  });
+}
+
+function renderQuiz(quizData) {
+  const quizForm = document.getElementById('quizForm');
+  quizForm.innerHTML = '';
+
+  quizData.forEach((q, index) => {
+    const count = index + 1;
+
+    const label = document.createElement('label');
+
+    label.textContent = 'Q' + count + ': ' + q.question;
+
+    quizForm.appendChild(label);
+
+    if (q.answers.length > 0) {
+      let type = '';
+
+      if (q.correct.length > 1) {
+        type = 'checkbox';
+      } else {
+        type = 'radio';
+      }
+
+      q.answers.forEach((a) => {
+        const input = document.createElement('input');
+        const inputLabel = document.createElement('label');
+
+        input.type = type;
+        input.value = a;
+        input.name = 'Q' + count;
+
+        inputLabel.textContent = a;
+        inputLabel.htmlFor = 'Q' + count;
+
+        quizForm.appendChild(input);
+        quizForm.appendChild(inputLabel);
+      });
+    } else {
+      const input = document.createElement('input');
+
+      input.type = 'text';
+      input.name = 'Q' + count;
+      input.value = '';
+
+      quizForm.appendChild(input);
+    }
+
+    quizForm.appendChild(document.createElement('br'));
+    quizForm.appendChild(document.createElement('br'));
+  });
+
+  const submit = document.createElement('input');
+
+  submit.type = 'submit';
+  submit.value = 'Submit';
+
+  quizForm.appendChild(submit);
+}
+
+function renderResults(data) {
+
+  const resultsPage = document.getElementById("resultsDisplay");
+  resultsPage.innerHTML = "";
+
+  const answers = JSON.parse(localStorage.getItem("answers"));
+
+  data.forEach((q, index) => {
+
+    const count = index + 1;
+
+    const h1 = document.createElement('h1');
+    h1.textContent = "Q" + count + ": " + q.question;
+
+    resultsPage.appendChild(h1);
+
+    const h2_1 = document.createElement("h2");
+    h2_1.textContent = "Your answer: ";
+
+    resultsPage.appendChild(h2_1);
+
+    const p1 = document.createElement("p");
+    p1.textContent = answers[index];
+
+    resultsPage.appendChild(p1);
+
+    const h2_2 = document.createElement("h2");
+    h2_2.textContent = "Correct answer: ";
+
+    resultsPage.appendChild(h2_2);
+
+    const p2 = document.createElement("p");
+    p2.textContent = q.correct;
+
+    resultsPage.appendChild(p2);
+
+  })
+}
+
 if (quiz) {
+
   document.getElementById('fileInput').addEventListener('change', function () {
     const file = this.files[0];
     const reader = new FileReader();
@@ -13,81 +124,11 @@ if (quiz) {
     reader.readAsText(file);
   });
 
-  // Do check to ensure that this works properly
-  function parseQuiz(text) {
-    const blocks = text.trim().split('\r\n\r\n');
-    console.log(blocks);
-    return blocks.map((block) => {
-      const lines = block.split('\r\n');
-      const question = lines.find((l) => l.startsWith('Q:')).slice(3);
-      const answers = lines.filter((l) => l.startsWith('A:')).map((a) => a.slice(3));
-      const correct = lines.filter((l) => l.startsWith('C:')).map((c) => c.slice(3));
-      return { question, answers, correct };
-    });
-  }
-
-  function renderQuiz(quizData) {
-    const quizForm = document.getElementById('quizForm');
-    quizForm.innerHTML = '';
-
-    quizData.forEach((q, index) => {
-      const count = index + 1;
-
-      const label = document.createElement('label');
-
-      label.textContent = 'Q' + count + ': ' + q.question;
-
-      quizForm.appendChild(label);
-
-      if (q.answers.length > 0) {
-        let type = '';
-
-        if (q.correct.length > 1) {
-          type = 'checkbox';
-        } else {
-          type = 'radio';
-        }
-
-        q.answers.forEach((a) => {
-          const input = document.createElement('input');
-          const inputLabel = document.createElement('label');
-
-          input.type = type;
-          input.value = a;
-          input.name = 'Q' + count;
-
-          inputLabel.textContent = a;
-          inputLabel.htmlFor = 'Q' + count;
-
-          quizForm.appendChild(input);
-          quizForm.appendChild(inputLabel);
-        });
-      } else {
-        const input = document.createElement('input');
-
-        input.type = 'text';
-        input.name = 'Q' + count;
-        input.value = '';
-
-        quizForm.appendChild(input);
-      }
-
-      quizForm.appendChild(document.createElement('br'));
-      quizForm.appendChild(document.createElement('br'));
-    });
-
-    const submit = document.createElement('input');
-
-    submit.type = 'submit';
-    submit.value = 'Submit';
-
-    quizForm.appendChild(submit);
-  }
-
   quiz.addEventListener('submit', function (e) {
     e.preventDefault();
 
     const data = JSON.parse(localStorage.getItem('quizData'));
+    const answers = {};
 
     let score = 0;
 
@@ -103,9 +144,13 @@ if (quiz) {
           const sameLength = userAnswer.length == q.correct.length;
           const sameValues = userAnswer.every((a) => q.correct.includes(a));
 
+          answers[index] = userAnswer;
+
           if (sameLength && sameValues) score++;
         } else {
           const userAnswer = e.target.querySelector("input[name='" + qName + "']:checked");
+
+          answers[index] = userAnswer.value;
 
           if (q.correct.includes(userAnswer.value)) score++;
         }
@@ -113,31 +158,40 @@ if (quiz) {
         const userAnswer = e.target.elements[qName].value.trim().toLowerCase();
         const correct = q.correct.map((c) => c.toLowerCase());
 
+        answers[index] = userAnswer;
+
         if (correct.includes(userAnswer)) score++;
       }
     });
 
     localStorage.setItem('score', score);
+    localStorage.setItem('answers', JSON.stringify(answers));    
 
     window.location.href = 'pages/results.html';
   });
 }
 
 // Results page
-const data = JSON.parse(localStorage.getItem('quizData'));
-const score = Number(localStorage.getItem('score'));
+const result = document.getElementsByClassName("results");
 
-const gif = document.getElementById('resultGif');
-const scoreMess = document.getElementById('resultScore');
+if(result) {
+  const data = JSON.parse(localStorage.getItem('quizData'));
+  const score = Number(localStorage.getItem('score'));
 
-if (gif && scoreMess) {
-  scoreMess.textContent = 'You scored ' + score + " out of " + data.length;
+  const gif = document.getElementById('resultGif');
+  const scoreMess = document.getElementById('resultScore');
 
-  if (score / data.length < 0.5) {
-    gif.src = '../assets/sad.gif';
-  } else if (score / data.length < 0.75) {
-    gif.src = '../assets/edp.gif';
-  } else {
-    gif.src = '../assets/rad.gif';
+  if (gif && scoreMess) {
+    scoreMess.textContent = 'You scored ' + score + " out of " + data.length;
+
+    if (score / data.length < 0.5) {
+      gif.src = '../assets/sad.gif';
+    } else if (score / data.length < 0.75) {
+      gif.src = '../assets/edp.gif';
+    } else {
+      gif.src = '../assets/rad.gif';
+    }
   }
+
+  renderResults(data);
 }
